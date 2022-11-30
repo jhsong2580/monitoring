f10ac3c6cb7315b11249b5174557c605e7c628db
jhsong2580 <thdwmdgns@naver.com>
2022-11-30 23:28:46 +0900

    feat: 3초 대기 API, BIGSIZE IMAGE API, database조회 API Endpoint 설정

--- module-application/src/main/java/monitoring/controller/ApplicationController.java
+++ module-application/src/main/java/monitoring/controller/ApplicationController.java
@@ -7,22 +7,31 @@ import monitoring.service.ApplicationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class ApplicationController {
 
     private final ApplicationService applicationService;
 
+    @ResponseBody
     @GetMapping("/api/database")
     public ResponseEntity<List<SectionDetailDTO>> sections (){
         return ResponseEntity.ok(applicationService.details());
     }
 
+    @ResponseBody
     @GetMapping("/api/sleep")
     public ResponseEntity  sleep(){
         applicationService.sleep();
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/api/image")
+    public String getImagePage(){
+        System.out.println("hi");
+        return "hello";
     }
 }
