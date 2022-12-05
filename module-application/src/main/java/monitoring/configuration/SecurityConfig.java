251d0e5eb933bb91ebce4be58254035b02b374bb
jhsong2580 <thdwmdgns@naver.com>
2022-12-05 14:36:53 +0900

    fix: actuator endpoint 환경변수 설정

--- module-application/src/main/java/monitoring/configuration/SecurityConfig.java
+++ module-application/src/main/java/monitoring/configuration/SecurityConfig.java
@@ -12,9 +12,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Value("${management.endpoint.auth_user}")
+    @Value("${actuator_endpoint_user}")
     private String user;
-    @Value("${management.endpoint.auth_password}")
+    @Value("${actuator_endpoint_password}")
     private String password;
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
