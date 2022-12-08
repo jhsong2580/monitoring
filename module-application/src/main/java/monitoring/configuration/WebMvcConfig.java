dedf094a4640522ec5b8b95e4a9ff37d9f504e54
jhsong2580 <thdwmdgns@naver.com>
2022-12-08 19:55:25 +0900

    style

--- module-application/src/main/java/monitoring/configuration/WebMvcConfig.java
+++ module-application/src/main/java/monitoring/configuration/WebMvcConfig.java
@@ -1,25 +1,25 @@
 package monitoring.configuration;
 
 import javax.servlet.Filter;
-import monitoring.configuration.interceptor.LogInterceptor;
 import org.springframework.boot.autoconfigure.web.WebProperties.Resources.Cache.Cachecontrol;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
     public static final String PREFIX_STATIC_RESOURCES = "/img";
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // img DIR내 파일 접근시 max-age를 1년으로 설정한다.
         registry.addResourceHandler("/img/**")
             .addResourceLocations("classpath:/static/img/")
-            .setCachePeriod(60*60*24*365);
+            .setCachePeriod(60 * 60 * 24 * 365);
 
         Cachecontrol cachecontrol = new Cachecontrol();
         cachecontrol.setCachePrivate(true);
