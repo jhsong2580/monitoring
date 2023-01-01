36ee7fabafb65ae10efe14b091869288764e1a40
jhsong2580 <thdwmdgns@naver.com>
2023-01-01 21:11:28 +0900

    feat: Body에서 Token 획득하는 ArgumentResolver추가

---
+++ module-api-auth/src/main/java/monitoring/argumentResolver/JwtTokenResolver.java
@@ -0,0 +1,36 @@
+package monitoring.argumentResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import monitoring.configuration.jwt.TokenExtractor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(JwtToken.class) &&
+            parameter.getParameterType().equals(String.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+        NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        String token = TokenExtractor.extract(
+            webRequest.getNativeRequest(HttpServletRequest.class));
+
+        if(ObjectUtils.isEmpty(token)){
+            throw new IllegalArgumentException("token required");
+        }
+
+        return token;
+    }
+}
