2e7390bc5ab47e5212a53bb7d09acbb631dbc32f
jhsong2580 <thdwmdgns@naver.com>
2023-01-01 17:13:13 +0900

    test: api-auth module context load

---
+++ module-api-auth/src/test/java/monitoring/ApiAuthApplicationTests.java
@@ -0,0 +1,12 @@
+package monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAuthApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
